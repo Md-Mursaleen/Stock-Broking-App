823293fa3fb85a06dd4a2b929bcc4dc8bfefe73a
Md Mursaleen <101120935+Md-Mursaleen@users.noreply.github.com>
2024-07-08 00:12:15 +0530

    update env.js

--- src/utilis/env.js
+++ src/utilis/env.js
@@ -1,10 +1,5 @@
 /* eslint-disable prettier/prettier */
 export const BASE_URL = 'https://api.twelvedata.com/time_series?';
-export const API_KEY = '547aed64afa04fbab683e63379f9c6b5';
-// export const ALPHA_API_KEY = 'K4Z2AG9I2IIPBABK';
-// export const ALPHA_API_KEY = 'J814I9I2KUX6DZHL';
-// export const ALPHA_API_KEY = 'VDV5G208SQGIEFIM';
-// export const ALPHA_API_KEY = 'GLSSO78G81E9UM1X';
-// export const ALPHA_API_KEY = 'OASF4OIPO65O4XSZ';
-export const ALPHA_API_KEY = 'AOXG8O71XM0YZ102';
+export const API_KEY = 'YOUR_API_KEY';
+export const ALPHA_API_KEY = 'YOUR_API_KEY';
 export const ALPHA_BASE_URL = 'https://www.alphavantage.co/query';
